9861382a888c7b8a846a9fdf303625d3799ca3e0
ADMIN <nikitosovui@gmail.com>
2026-01-31 13:44:46 +0800

    pre-release

--- config.py
+++ config.py
@@ -1,8 +1,10 @@
 import os
 from dotenv import load_dotenv
+from typing import List
 
 load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 CHANNEL_ID = int(os.getenv("CHANNEL_ID"))
-ADMIN_ID = int(os.getenv("ADMIN_ID"))
+ADMIN_IDS_STR = os.getenv("ADMIN_ID", "")
+ADMIN_ID: List[int] = [int(x.strip()) for x in ADMIN_IDS_STR.split(",") if x.strip()]
